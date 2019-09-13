edf8003dae6be84634277b8855c84aa5ba174a8e
Sopan <sopan.mittal@indiqube.com>
2019-09-13 17:29:41 +0530

    Using Service for cross component communitation

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
-export class RecipeService {
+export class RecipeService {Ingredient
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('Corn Chat', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800'),
     new Recipe('Corn Chat 1', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800'),
