af35ed122a99afa34c74605bb971a8aba9bea34d
Sopan <sopan.mittal@indiqube.com>
2019-09-16 16:47:27 +0530

    Services & Dependency Injection

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,25 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
-export class RecipeService {Ingredient
+@Injectable()
+export class RecipeService {
+  Ingredient
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Corn Chat', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800'),
-    new Recipe('Corn Chat 1', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800'),
-    new Recipe('Corn Chat 2', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800')
+    new Recipe('Corn Chat', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800', [new Ingredient('Corn', 1), new Ingredient('Salt', 1)]),
+    new Recipe('Corn Chat 1', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800', [new Ingredient('Corn', 2), new Ingredient('Peper', 1)]),
+    new Recipe('Corn Chat 2', 'Some Description', 'https://recipes.timesofindia.com/thumb/59215318.cms?imgsize=331377&width=800&height=800', [new Ingredient('Corn', 3), new Ingredient('Lemon', 1)])
   ];
+
+  constructor(private slService: ShoppingListService){}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
